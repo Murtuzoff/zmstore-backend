0a2c6fe1e66f33a7d1a3acd36a23ed63394eb2d0
Murtuzoff <r.murtuzoff@gmail.com>
2023-12-08 13:57:41 +0300

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import config from "dotenv/config";
+// import config from "dotenv/config";
 import connectDatabase from "./config/database.js";
 import express from "express";
 // import cors from "cors";
