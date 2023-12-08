be91b77194596ec178bc27b5febb3e0e4de866f3
Murtuzoff <r.murtuzoff@gmail.com>
2023-12-08 14:22:23 +0300

    test

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize";
+import pg from "pg";
 
 const sequelize = new Sequelize(
   process.env.DB_NAME, // база данных
@@ -6,6 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS, // пароль
   {
     dialect: "postgres",
+    dialectModule: pg,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialectOptions: {
