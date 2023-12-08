b629fb907a0edd07e21ffcfdefd8b0efca638937
Murtuzoff <r.murtuzoff@gmail.com>
2023-12-08 14:46:11 +0300

    test

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,19 +1,15 @@
 import express from "express";
-// import { protect, admin } from "./../middleware/Authorization.js";
-// import productController from "../controllers/productController.js";
+import { protect, admin } from "./../middleware/Authorization.js";
+import productController from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
-productRouter.get("/", async (req, res) => {
-  res.json({ message: "Hello, world!" });
-});
-
-// productRouter.get("/all", protect, admin, productController.allAdmin);
-// productRouter.get("/", productController.all);
-// productRouter.get("/:id", productController.single);
-// productRouter.post("/create", protect, admin, productController.create);
-// productRouter.post("/:id/review", protect, productController.review);
-// productRouter.put("/:id/update", protect, admin, productController.update);
-// productRouter.delete("/:id/delete", protect, admin, productController.delete);
+productRouter.get("/all", protect, admin, productController.allAdmin);
+productRouter.get("/", productController.all);
+productRouter.get("/:id", productController.single);
+productRouter.post("/create", protect, admin, productController.create);
+productRouter.post("/:id/review", protect, productController.review);
+productRouter.put("/:id/update", protect, admin, productController.update);
+productRouter.delete("/:id/delete", protect, admin, productController.delete);
 
 export default productRouter;
