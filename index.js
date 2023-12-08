c65eb5994c6263b2e2ba45db91365f5f4b6c4764
Murtuzoff <r.murtuzoff@gmail.com>
2023-12-08 13:54:10 +0300

    test

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ import mainRouter from "./routes/mainRouter.js";
 import fileUpload from "express-fileupload";
 import ErrorHandler from "./middleware/ErrorHandler.js";
 
-connectDatabase();
+// connectDatabase();
 const app = express();
-app.use(cors());
-app.use(express.static("static"));
-app.use(fileUpload());
-app.use(express.json());
-app.use("/api", mainRouter);
-app.use(ErrorHandler);
+// app.use(cors());
+// app.use(express.static("static"));
+// app.use(fileUpload());
+// app.use(express.json());
+// app.use("/api", mainRouter);
+// app.use(ErrorHandler);
+
+app.get("/api", async (req, res) => {
+  res.json({ message: "Hello, world!" });
+});
 
 const PORT = process.env.PORT || 7000;
 
