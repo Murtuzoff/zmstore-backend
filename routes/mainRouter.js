f0506950e747e09bfb0452a3bcc5ed5ab7aabbf2
Murtuzoff <r.murtuzoff@gmail.com>
2023-12-08 14:10:57 +0300

    test

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,14 +1,14 @@
 import express from "express";
 import orderRouter from "./orderRouter.js";
-// import PayPalRouter from "./PayPalRouter.js";
-// import productRouter from "./productRouter.js";
-// import userRouter from "./userRouter.js";
+import PayPalRouter from "./PayPalRouter.js";
+import productRouter from "./productRouter.js";
+import userRouter from "./userRouter.js";
 
 const mainRouter = express.Router();
 
-// mainRouter.use("/order", orderRouter);
-// mainRouter.use("/paypal", PayPalRouter);
-// mainRouter.use("/product", productRouter);
-// mainRouter.use("/user", userRouter);
+mainRouter.use("/order", orderRouter);
+mainRouter.use("/paypal", PayPalRouter);
+mainRouter.use("/product", productRouter);
+mainRouter.use("/user", userRouter);
 
 export default mainRouter;
